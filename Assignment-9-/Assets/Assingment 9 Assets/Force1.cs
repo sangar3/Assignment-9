904e1491e8cf0c4ffcf3da8e5f11ed53d9b485d4
sangar3 <55110746+sangar3@users.noreply.github.com>
2019-11-08 17:45:35 -0600

    added door

---
+++ Assignment-9-/Assets/Assingment 9 Assets/Force1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Force1 : MonoBehaviour
+{
+    private Rigidbody playerRB;
+
+    public float ForceMult = 200;
+
+    public ForceMode forceMode;
+
+    private void awake()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    
+    void OnTriggerStay(Collider other)
+    {
+        if (other.attachedRigidbody)
+            other.attachedRigidbody.AddForce(transform.up * ForceMult * Time.deltaTime);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        transform.Translate(Vector3.up * Time.deltaTime);
+    }
+
+
+}
